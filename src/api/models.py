b83b9a8ad9cab3d663ba14ed37f5257f3118d21c
SGuevaraClark <sebastianguevarac@gmail.com>
2025-01-31 15:03:18 +0000

    books

--- src/api/models.py
+++ src/api/models.py
@@ -8,6 +8,8 @@ class Users(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    address_id = db.Column(db.Integer, db.ForeignKey('addresses.id'))
+    address = db.relationship('Addresses', backref='user', uselist= False) # esto devuelve un objeto
 
     def __repr__(self):
         return f'<Users {self.email}>'
@@ -17,9 +19,75 @@ class Users(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+            "address": self.address.serialize() if self.address else None
         }
     
 
- 
+class Addresses(db.Model):
+    __tablename__ = 'addresses'
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return f'<Address {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            # do not serialize the password, its a security breach
+            "address": self.address
+        }
 
 
+class Authors(db.Model):
+    __tablename__ = 'authors'
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(250), nullable=False)
+    books = db.relationship('Books', backref='author', lazy= True)
+
+    def __repr__(self):
+        return f'<Authors {self.full_name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            # do not serialize the password, its a security breach
+            "full_name": self.full_name,
+            "books": [book.serialize() for book in self.books] if self.books else None
+        }
+    
+
+class Books(db.Model):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable= False)
+    book_category = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable= False)
+
+    def __repr__(self):
+        return f'<Books {self.title}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            # do not serialize the password, its a security breach
+            "title": self.title
+        }
+    
+
+
+class Categories(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(250), nullable=False)
+    book_category = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable= False)
+
+    def __repr__(self):
+        return f'<Books {self.title}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            # do not serialize the password, its a security breach
+            "title": self.title
+        }
